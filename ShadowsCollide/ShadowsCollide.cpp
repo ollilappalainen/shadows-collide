ba5a94c22d8e1e44378cf30cc93d7e318db69ee4
Olli Lappalainen <masterhillo@gmail.com>
2019-04-03 23:38:56 +0300

    Add resolve if square and work on convex recognizion

--- ShadowsCollide/ShadowsCollide.cpp
+++ ShadowsCollide/ShadowsCollide.cpp
@@ -2,6 +2,7 @@
 //
 
 #include "pch.h"
+#include "Vector.h"
 #include <iostream>
 #include <sstream>
 #include <algorithm>
@@ -10,12 +11,6 @@
 
 using namespace std;
 
-struct Point
-{
-	// todo: add move this to geometries directory and change vector to Points
-	int x, y;
-};
-
 int getArraySizeFromVectors(const string vectorString)
 {
 	size_t arraySize = count(vectorString.begin(), vectorString.end(), ' ') + 1;
@@ -23,16 +18,19 @@ int getArraySizeFromVectors(const string vectorString)
 	return arraySize;
 }
 
-const vector<double> mutateStringToVector(const string vectorString)
+const Vector mutateStringToVector(const string vectorString)
 {
-	vector<double> vect;
+	Vector vect;	
+	vect.x = NULL;
+	vect.y = NULL;
+
 	stringstream ss(vectorString);
 
 	double i;
 
 	while (ss >> i)
 	{
-		vect.push_back(i);
+		vect.x == NULL ? vect.x = i : vect.y = i;
 
 		if (ss.peek() == ',')
 		{
@@ -43,9 +41,9 @@ const vector<double> mutateStringToVector(const string vectorString)
 	return vect;
 }
 
-const vector<vector<double>> mutateVectorsStringToArray(const string vectorString)
+const vector<Vector> mutateVectorsStringToArray(const string vectorString)
 {
-	vector<vector<double>> vectors;
+	vector<Vector> vectors;
 	stringstream ss(vectorString);
 	string i;
 	vector<string> vectorTemp;
@@ -58,50 +56,155 @@ const vector<vector<double>> mutateVectorsStringToArray(const string vectorStrin
 	return vectors;
 }
 
-bool isQuadrangle(const vector<vector<double>> vectors)
+double countDistance(Vector p, Vector q)
 {
-	bool isQuandrangle;
+	return (p.x - q.x) * (p.x - q.x) + (p.y - q.y) * (p.y - q.y);
+}
 
-	if (vectors.size() < 4 || vectors.size() > 4) {
-		isQuandrangle = false;
+double countSlope(Vector v1, Vector v2)
+{
+	double dx, dy, slope;
+
+	dx = v2.x - v1.x;
+	dy = v2.y - v1.y;
+	slope = dy / dx;
+
+	return slope;
+}
+
+bool isAABB(const vector<Vector> vectors)
+{
+	bool isAABB;
+
+	if (vectors.size() != 4) {
+		isAABB = false;
+	}
+	else {
+		Vector vect1 = vectors.at(0);
+		Vector vect2 = vectors.at(1);
+		Vector vect3 = vectors.at(2);
+		Vector vect4 = vectors.at(3);
+
+		// Check if opposite sides of quadrangle are same length
+		double dist12 = countDistance(vect1, vect2);
+		double dist34 = countDistance(vect3, vect4);
+		double dist23 = countDistance(vect2, vect3);
+		double dist41 = countDistance(vect4, vect1);
+
+		if (dist12 == dist34 && dist23 == dist41) {
+			// Check if slopes are 0
+			double slope1, slope2, slope3, slope4;
+
+			slope1 = countSlope(vect1, vect2);
+			slope2 = countSlope(vect2, vect3);
+			slope3 = countSlope(vect3, vect4);
+			slope4 = countSlope(vect4, vect1);
+
+			// If opposite slopes are same and all of them are 0
+			isAABB = fabs(slope1) == fabs(slope3) && 
+				fabs(slope2) == fabs(slope4) && 
+				(isinf(slope1) || slope1 == 0) && 
+				(isinf(slope2) || slope2 == 0) && 
+				(isinf(slope3) || slope3 == 0) && 
+				(isinf(slope4) || slope4 == 0);
+		}
+		else
+		{
+			isAABB = false;
+		}
 	}
 
-	for (int i = 0; i < 4; i++)
+	return isAABB;
+}
+
+bool isConvexPolygon(const vector<Vector> vectors)
+{
+	bool isConvex = false;
+
+	for (int i = 0; i < vectors.size(); i++)
 	{
-		double dx, dy, slope;
-		int index1 = 0;
-		int index2 = 1;
+		// figure this out..????
+		/*int vectorsLastIndex = vectors.size() - 1;
+		int middlePoint = i + 1;
+		int endPoint1 = middlePoint - 1;
+		int endPoint2 = middlePoint + 1;
 
-		dx = vectors.at(index2).at(0) - vectors.at(index1).at(0);
-		dy = vectors.at(index2).at(1) - vectors.at(index1).at(1);
-		slope = dy / dx;
+		if (i == vectors.size())
+		{
+			middlePoint = 0;
+			endPoint2 = 1;
+		}
+		else if (middlePoint == 0)
+		{
+			endPoint1 = vectorsLastIndex;
+		}
+		else if (middlePoint == vectorsLastIndex)
+		{
+			endPoint2 = 0;
+		}
 
-		isQuandrangle = (vectors.at(index1).at(0) == vectors.at(index2).at(0) || vectors.at(index1).at(1) == vectors.at(index2).at(1)) && isinf(slope);
+		Vector p1 = vectors.at(middlePoint);
+		Vector p2 = vectors.at(endPoint1);
+		Vector p3 = vectors.at(endPoint2);
+
+		float dx21 = p2.x - p1.x;
+		float dx31 = p3.x - p1.x;
+		float dy21 = p2.y - p1.y;
+		float dy31 = p3.y - p1.y;
+		float m12 = sqrt(dx21*dx21 + dy21 * dy21);
+		float m13 = sqrt(dx31*dx31 + dy31 * dy31);
+		float theta = acos((dx21*dx31 + dy21 * dy31) / (m12 * m13));*/
 	}
 
-	return isQuandrangle;
+	return isConvex;
+}
+
+bool isOBB(const vector<Vector> vectors)
+{
+	bool isOBB = false;
+	   
+	return isOBB;
+}
+
+void validateVectors(string vectors)
+{
+
 }
 
 int main()
 {
 	// -2,-2 -2,3 3,3 3,-2
 	string vectorsString;
-    cout << "Please enter a list of X and Y vectors" << endl; 
+    cout << "Please enter a list of two or more X and Y vectors." << endl; 
+	cout << "Vectors must form a convex polygon." << endl;
 	cout << "Valid form: x,y x,y x,y ..." << endl;
 	cout << "Example: -4.46,12.71 2.61,12.52 5.21,10.57 -3.46,8.71" << endl;
+	cout << "Press CTRL + C to end program." << endl;
+	cout << "Vectors: ";
 	getline(cin, vectorsString);
 
 	int vectorsCount = getArraySizeFromVectors(vectorsString);
 
-	const vector<vector<double>> vectors = mutateVectorsStringToArray(vectorsString);
+	if (vectorsCount > 1) {
+		const vector<Vector> vectors = mutateVectorsStringToArray(vectorsString);
 
-	if (isQuadrangle(vectors))
-	{
-		cout << "Polygon is quadrangle" << endl;
+		isConvexPolygon(vectors);
+
+		if (isAABB(vectors))
+		{
+			cout << "Polygon is quadrangle" << endl;
+		}
+		else
+		{
+			cout << "Please enter quadangle.." << endl;
+			cout << "Vectors: ";
+			getline(cin, vectorsString);
+		}
 	}
-	else
-	{
-		cout << "Please enter quadangle.." << endl;
+	else {
+		cout << "Please enter more than one vector." << endl;
+		cout << "Vectors: ";
+		getline(cin, vectorsString);
 	}
 
 	while (cin.good()) {
--- ShadowsCollide/pch.h
+++ ShadowsCollide/pch.h
@@ -8,6 +8,7 @@
 
 #ifndef PCH_H
 #define PCH_H
+#define _USE_MATH_DEFINES
 
 // TODO: add headers that you want to pre-compile here
 
